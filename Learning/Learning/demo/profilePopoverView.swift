0ce79ac0332292aca2167aa1223ec5902af4d31c
baptxste <baptiste.chachura@me.com>
2026-05-06 22:36:19 +0200

    cleaning

---
+++ Learning/Learning/demo/profilePopoverView.swift
@@ -0,0 +1,18 @@
+//
+//  profilePopoverView.swift
+//  Learning
+//
+//  Created by Baptiste Chachura on 06/05/2026.
+//
+
+import SwiftUI
+
+struct profilePopoverView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    profilePopoverView()
+}
